805465bbc9565467f23607d45e289f93dc3b0ad0
aleccalvo08 <aleccalvo@Alecs-MacBook-Pro.local>
2018-04-25 10:21:00 -0400

    Publishing to Github

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: './'
+  })
+})
+
+gulp.task('build-css', function() {
+  return gulp.src('style.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('css'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch("style.scss", ['build-css', reload])
+  gulp.watch('*.html').on('change', reload)
+})
+
+gulp.task('default', ['server', 'watch'])
